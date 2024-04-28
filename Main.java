795223010cedd3b3c12901e20303e1347f583220
larswenson <larsswenson@seattlecolleges.edu>
2024-04-28 14:37:54 -0700

    Initial commit

---
+++ Main.java
@@ -0,0 +1,100 @@
+abstract class Shape {
+    public abstract double calculateArea();
+    public abstract double calculatePerimeter();
+    public abstract String getName();
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String getName() {
+        return "Circle";
+    }
+}
+
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+
+    @Override
+    public String getName() {
+        return "Rectangle";
+    }
+}
+
+class Triangle extends Shape {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    @Override
+    public double calculateArea() {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return side1 + side2 + side3;
+    }
+
+    @Override
+    public String getName() {
+        return "Triangle";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4, 6);
+        Triangle triangle = new Triangle(3, 4, 5);
+
+        printShapeDetails(circle);
+        printShapeDetails(rectangle);
+        printShapeDetails(triangle);
+    }
+
+    private static void printShapeDetails(Shape shape) {
+        System.out.println("Name: " + shape.getName());
+        System.out.println("Area: " + shape.calculateArea());
+        System.out.println("Perimeter: " + shape.calculatePerimeter());
+        System.out.println();
+    }
+}
